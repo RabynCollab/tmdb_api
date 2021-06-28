b4ec3638aa3c354de5895827b066f5e3b1f62e55
Rabyn900 <85889572+RabynCollab@users.noreply.github.com>
2021-06-28 22:21:48 +0545

    Update Up.js

--- src/movies_navs/Up.js
+++ src/movies_navs/Up.js
@@ -3,7 +3,7 @@ import PopularMovies from '../movies_provider/PopularMovies';
 import Upcoming from '../navs/Upcoming';
 
 const Up = () => {
-  const { data, loading } = PopularMovies('https://api.themoviedb.org/3/movie/upcoming?api_key=2a0f926961d00c667e191a21c14461f8&language=en-US&page=1');
+  const { data, loading } = PopularMovies('https://api.themoviedb.org/3/movie/upcoming?api_key=2a0f926961d00c6691a2c14461f8&language=en-US&page=1');
   return (
     <div>
       {data && <Upcoming movies={data} />}
