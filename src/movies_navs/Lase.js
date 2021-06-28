454faa570e73f9d8713b1162499f585ee036d2ef
Rabyn900 <85889572+RabynCollab@users.noreply.github.com>
2021-06-28 22:21:27 +0545

    Update Lase.js

--- src/movies_navs/Lase.js
+++ src/movies_navs/Lase.js
@@ -3,7 +3,7 @@ import PopularMovies from '../movies_provider/PopularMovies';
 import Latest from '../navs/Latest';
 
 const Lase = () => {
-  const { data, loading } = PopularMovies('https://api.themoviedb.org/3/movie/top_rated?api_key=2a0f926961d00c667e191a21c14461f8&language=en-US&page=1');
+  const { data, loading } = PopularMovies('https://api.themoviedb.org/3/movie/top_rated?api_key=2a0f926961d00c667e1c14461f8&language=en-US&page=1');
   return (
     <div>
       {data && <Latest movies={data} />}
